8ebcebe08d3fd622c0e2d0a5a09d7c084e6bea3f
Armard Bellamy <armardbellamy@gmail.com>
2016-07-10 22:52:20 -0400

    Configured email confirmation functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,8 +52,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'localhost', port: 3000 }
-
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+
+  
 
 end
