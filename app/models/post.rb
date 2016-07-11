0a2dd52ade1402a163a990965b6f9a0fa19aeeb0
Armard Bellamy <armardbellamy@gmail.com>
2016-07-11 12:35:53 -0400

    Added Amazon S3 for paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,7 @@ class Post < ApplicationRecord
 	# before_save :upload_to_s3
 
 	has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" },
-     :default_url => "/images/:style/missing.png",
-     :url => ":s3_domain_url",
-     :path => "public/posts/:id/:style_basename.:extension",
-     :storage => :fog,
-     :fog_credentials => {
-        provider: 'AWS',
-        aws_access_key_id: "#{ENV["AWS_ACCESS_KEY_ID"]}",
-        aws_secret_access_key: "#{ENV["AWS_SECRET_ACCESS_KEY"]}"
-     }, 
-     fog_directory: ENV["FOG_DIRECTORY"]
+     :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
